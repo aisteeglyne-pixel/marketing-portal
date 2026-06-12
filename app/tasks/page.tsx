f46d753a951dd43ea7b5eb1bcc1073e0df426ff8
Aistė Eglynė <aiste.eglyne@gmail.com>
2026-06-12 12:42:55 +0300

    chore: portal sync (cleanup legacy pages, content block fixes)

--- app/tasks/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-export default function TasksPage() {
-  const router = useRouter()
-  useEffect(() => { router.replace('/dashboard') }, [])
-  return null
-}
